329f619e99d4b6c1c715cad5b9f9a785cc418f3b
MaximMn04 <maximmorales10@gmail.com>
2026-05-06 19:08:33 +0200

    Methods added to strip and add depots to routes in order implement localSearch

---
+++ localSearch.py
@@ -0,0 +1,34 @@
+from InstanceCVRPTWUI import InstanceCVRPTWUI
+from greedyBaseline import build_dist_matrix, assign_delivery_days
+from routingParallel import build_routes_parallel_regret
+
+def strip_depots(days_routes):
+    no_depot_route = {key: [route[1:-1] for route in day_routes] for key, day_routes in days_routes.items()}
+    return no_depot_route
+
+
+def add_depots(days_routes):
+    route_with_depot = {key: [[0] + route + [0] for route in day_routes] for key, day_routes in days_routes.items()}
+    return route_with_depot
+
+if __name__ == "__main__":
+    instance_path = "B1.txt"
+    
+    inst = InstanceCVRPTWUI(instance_path)
+    inst.calculateDistances()
+    dist = build_dist_matrix(inst)
+    
+    delivery_day = assign_delivery_days(inst)
+    days_routes = build_routes_parallel_regret(inst, delivery_day, dist)
+    stripped_routes = strip_depots(days_routes)
+    added_depot_routes = add_depots(stripped_routes)
+    if added_depot_routes == days_routes:
+        print("Test passed: Stripping and adding depots returns original routes.")
+    else:
+        print("Test FAILED: round-trip did not match original.")
+    
+    # your test here:
+    # 1. strip the depots
+    # 2. add them back
+    # 3. assert the result equals the original
+    # 4. print something so you know it ran
